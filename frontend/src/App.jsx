8dc46967b32b76b1fdaaa232560338cfddd4a3e4
Hitesh patidar <168549534+hiteshpatidar009@users.noreply.github.com>
2025-03-21 22:58:49 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from './components/home/Home'
+import Home from './components/Home/Home'
 import { Navigate, Route , Routes } from "react-router-dom"
 import Productes from './components/Products/Productes';
 import Signup from './components/Signup';
